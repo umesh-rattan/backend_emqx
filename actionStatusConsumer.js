7e88a5fc50ad9e20464762b6e4ab378c857961af
developer <developer@revoltmotors.com>
2026-04-02 12:04:21 +0530

    action status fixed

--- actionStatusConsumer.js
+++ actionStatusConsumer.js
@@ -5,7 +5,10 @@ const db = require('./actionStore');
 client.on('message', async (topic, payload) => {
   if (!topic.includes('/action/status')) return;
 
-  const statuses = JSON.parse(payload.toString());
+  let statuses = JSON.parse(payload.toString());
+  if (!Array.isArray(statuses)) {
+    statuses = [statuses];
+  }
 
   for (const status of statuses) {
     await db.updateActionStatus(status, topic);
